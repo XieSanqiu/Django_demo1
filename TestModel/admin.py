818915aff5bdb863d7da5a2a2937de3181d7b2ec
XieSanqiu <xll_sanqiu@163.com>
2020-10-28 20:15:19 +0800

    feat:Django Admin管理工具简单学习使用

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from TestModel.models import Test, Contact, Tag
+
 # Register your models here.
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'email')  # list显示更多属性
+
+    search_fields = ('name',)  #增加搜索栏
+
+    inlines = [TagInline]  # 内联显示
+
+    # fields = ('name', 'email')  #自定义表单，只显示部分属性
+    fieldsets = (
+        ['Main', {
+            'fields': ('name', 'email'),
+        }],
+        ['Advance', {
+            'classes': ('collapse',),  # CSS
+            'fields': ('age',),
+        }]
+    )  #属性分块显示
+
+admin.site.register(Contact, ContractAdmin)
+# admin.site.register([Test, Tag]) #为了内联显示
+admin.site.register([Test])
--- TestModel/models.py
+++ TestModel/models.py
@@ -6,3 +6,20 @@ class Test(models.Model):
     age = models.IntegerField()
     def __str__(self):
         return 'name:'+self.name+'; age:' + str(self.age)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, )
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
